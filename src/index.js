3ecbfdb1c530d33eca8c261245055d2443a60aef
Lunik <guillaume.lunik@gmail.com>
2016-05-11 19:25:36 +0200

    add scan method

--- src/index.js
+++ src/index.js
@@ -2,28 +2,33 @@ var fs = require('fs')
 
 var ns = require('./NetworkTool.js')
 
-ns('192.168.1.0', '255.255.255.0', function(data){
-  fs.readFile(__dirname+'/db.json', function(err, file){
-    if(err) console.log(err)
+function NetworkStats (ip, mask){
+  this.network = ip
+  this.mask = mask
+}
 
-    file = JSON.parse(file)
-    for(var key in data){
-      var device = data[key]
-      if(typeof file[device.mac] === 'undefined'){
-        file[device.mac] = { name: "none", ip: {} }
-      }
-      if(typeof file[device.mac].ip === 'undefined'){
-        file[device.mac] = {}
-      }
-      if(typeof file[device.mac].ip[device.ip] === 'undefined'){
-        file[device.mac].ip[device.ip] = []
-      }
-      file[device.mac].ip[device.ip].push((new Date()).getTime())
-    }
-    fs.writeFile(__dirname+'/db.json', JSON.stringify(file, null, 2), function(err){
+NetworkStats.prototype.scan = function () {
+  ns(this.network, this.mask, function(data){
+    fs.readFile(__dirname+'/db-' + this.network + '.json', function(err, file){
       if(err) console.log(err)
+
+      file = JSON.parse(file)
+      for(var key in data){
+        var device = data[key]
+        if(typeof file[device.mac] === 'undefined'){
+          file[device.mac] = { name: "none", ip: {} }
+        }
+        if(typeof file[device.mac].ip === 'undefined'){
+          file[device.mac] = {}
+        }
+        if(typeof file[device.mac].ip[device.ip] === 'undefined'){
+          file[device.mac].ip[device.ip] = []
+        }
+        file[device.mac].ip[device.ip].push((new Date()).getTime())
+      }
+      fs.writeFile(__dirname+'/db.json', JSON.stringify(file, null, 2), function(err){
+        if(err) console.log(err)
+      })
     })
   })
-})
-
-function
+}
