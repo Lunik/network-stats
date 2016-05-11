f8a784c125dc8d499b89c14401bc3b3889836ec9
Lunik <guillaume.lunik@gmail.com>
2016-05-11 19:03:17 +0200

    write into db.json

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ var ns = require('./NetworkStats.js')
 
 ns('192.168.1.0', '255.255.255.0', function(data){
   fs.readFile(__dirname+'/db.json', function(err, file){
+    if(err) console.log(err)
+
     file = JSON.parse(file)
     for(var key in data){
       var device = data[key]
-      if(typeof file[device.mac] === 'undefined')
-      file[device.mac][device.ip].push(new Date())
+      if(typeof file[device.mac] === 'undefined'){
+        file[device.mac] = { name: "none", ip: {} }
+      }
+      if(typeof file[device.mac].ip === 'undefined'){
+        file[device.mac] = {}
+      }
+      if(typeof file[device.mac].ip[device.ip] === 'undefined'){
+        file[device.mac].ip[device.ip] = []
+      }
+      file[device.mac].ip[device.ip].push((new Date()).getTime())
     }
-    console.log(file)
+    fs.writeFile(__dirname+'/db.json', JSON.stringify(file), function(err){
+      if(err) console.log(err)
+    })
   })
 })
